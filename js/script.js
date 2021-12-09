ded3c9c1862eb5a0c99473f89a468b74a533b09a
Cezar <cezarlantosgmail.com>
2021-12-09 13:51:20 +0100

    click

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
+const priceperkm = 0.21;
+
 const outputHtml = document.getElementById('output');
 
-const distanza = prompt('Digitare il numero di chilometri che vuole percorrere')
+const distanza = prompt('Digitare il numero di chilometri che vuole percorrere');
 const age = prompt('Inserire la propria età')
 
-let prezzo = 0.21 * distanza;
+let prezzo = priceperkm * distanza;
 
+const button = document.getElementById('button-box');
+
+button.addEventListener('click', function () {
+    
 if (age <= 17) {
     prezzo = prezzo-( ( prezzo / 100) * 20);
     outputHtml.innerHTML = `Congratulazioni, avete ricevuto uno sconto del 20%. Prezzo biglietto: ${prezzo}€`;
@@ -12,5 +18,6 @@ if (age <= 17) {
     prezzo = prezzo-( ( prezzo / 100) * 40);
     outputHtml.innerHTML = `Congratulazioni, avete ricevuto uno sconto del 40%. Prezzo biglietto: ${prezzo}€`;
 } else {
-    outputHtml.innerHTML = `Prezzo biglietto: ${prezzo}`;
+    outputHtml.innerHTML = `Prezzo biglietto: ${prezzo}€`;
 }
+})
